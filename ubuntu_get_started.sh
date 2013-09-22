e29feb8d719a7b5ed0aea1678d515f3588af0b7f
Edward <edthedev@gmail.com>
2013-09-21 21:49:50 -0500

    Added a diagram of the circuit board for the bar graph project.

--- ubuntu_get_started.sh
+++ ubuntu_get_started.sh
@@ -1,4 +1,5 @@
 sudo apt-get install arduino arduino-core 
 sudo apt-get install picocom # For serial communication
+sudo apt-get install fritzing # For creating Arduino wiring diagrams to share.
 
 sudo adduser `whoami` dialout
