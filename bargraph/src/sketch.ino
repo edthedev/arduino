cf9e23cf945899de0573accc70710c59e485bb23
Edward <edthedev@gmail.com>
2013-09-21 19:02:08 -0500

    Up and down, worked on the first try!

--- bargraph/src/sketch.ino
+++ bargraph/src/sketch.ino
@@ -1,8 +1,44 @@
 
+# define LED_PIN 13
+# define LOW_PIN 3
+# define TOP_PIN 12
 void setup()
 {
+	pinMode(LED_PIN, OUTPUT);
+	
+	for(int pin=LOW_PIN; pin<=TOP_PIN; pin++)
+	{
+		pinMode(pin, OUTPUT);
+	}
 }
 
 void loop()
 {
+	digitalWrite(LED_PIN, HIGH);
+    delay(100);
+	digitalWrite(LED_PIN, LOW);
+
+	for(int pin=LOW_PIN; pin<=TOP_PIN; pin++)
+	{
+		digitalWrite(pin, HIGH);
+		delay(900);
+	}
+
+	for(int pin=TOP_PIN; pin>=LOW_PIN; pin--)
+	{
+		digitalWrite(pin, LOW);
+		delay(100);
+	}
 }
+/*
+##	for(int i=9; i<13; i++)
+##	{
+##		digitalWrite(i, HIGH);
+##	}
+##    delay(100);
+##	for(int i=9; i<13; i++)
+##	{
+##		digitalWrite(i, LOW);
+##	}
+##    delay(900);
+#*/
