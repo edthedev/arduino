83feb1d4e4131d7283cfbdc2de72d75d3a1784ea
Edward <edthedev@gmail.com>
2013-09-21 20:55:25 -0500

    Ignore the build directories.

--- bargraph/src/sketch.ino
+++ bargraph/src/sketch.ino
@@ -4,7 +4,6 @@
 # define TOP_PIN 12
 
 # define SWITCH 2
-int val = 0;
 
 void setup()
 {
@@ -23,31 +22,43 @@ void setup()
 
 void do_work()
 {
-	val = digitalRead(SWITCH);	
-	Serial.println(val);
+	// val = digitalRead(SWITCH);	
 }
+
+int val = 0;
+int switch_val = 0;
+int next_pin = 0;
 
 void loop()
 {
+	switch_val = digitalRead(SWITCH);	
+	Serial.println(switch_val);
+
+	if(switch_val)
+	{
+		digitalWrite(next_pin, HIGH);
+		next_pin++;
+		if(next_pin > TOP_PIN)
+		{
+			next_pin = TOP_PIN;
+		}
+	}
+	else
+	{
+		digitalWrite(next_pin, LOW);
+		next_pin--;
+		if(next_pin < LOW_PIN)
+		{
+			next_pin = LOW_PIN;
+		}
+	}
+	delay(100);
 
 	digitalWrite(LED_PIN, HIGH);
     delay(100);
 	digitalWrite(LED_PIN, LOW);
 
-	for(int pin=LOW_PIN; pin<=TOP_PIN; pin++)
-	{
-		do_work();
-		digitalWrite(pin, HIGH);
-		delay(900);
 	}
-
-	for(int pin=TOP_PIN; pin>=LOW_PIN; pin--)
-	{
-		do_work();
-		digitalWrite(pin, LOW);
-		delay(100);
-	}
-}
 /*
 ##	for(int i=9; i<13; i++)
 ##	{
