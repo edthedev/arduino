8da89125ff1249d2e04c6cebad61ab9c42e59d9c
Edward <edthedev@gmail.com>
2013-09-26 08:11:09 -0500

    Working recipe! Thanks arduinobasics.blogspot.com.

--- distance_sensor/src/sketch.ino
+++ distance_sensor/src/sketch.ino
@@ -1,52 +1,62 @@
-/* ir-distance sketch
- * prints distance and changes LED flash rate based on distance from IR sensor
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com/
+ on 10 Nov 2012.
  */
 
-const int ledPin    = 13; // the pin connected to the LED to flash
-const int sensorPin = 0;  // the analog pin connected to the sensor
 
-const long referenceMv = 5000; // long int to prevent overflow when multiplied
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+#define LEDPin 13 // Onboard LED
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+void setup() {
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 }
 
-void loop()
-{
-  int val = analogRead(sensorPin);
-  int mV = (val * referenceMv) / 1023;
+void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
 
-  Serial.print(mV);
-  Serial.print(",");
-  int cm = getDistance(mV);
-  Serial.println(cm);
-
-  digitalWrite(ledPin, HIGH);
-  delay(cm * 10 ); // each centimeter adds 10 milliseconds delay
-  digitalWrite(ledPin, LOW);
-  delay( cm * 10);
-
-  delay(100);
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ digitalWrite(LEDPin, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
 }
 
-// the following is used to interpolate the distance from a table
-// table entries are distances in steps of 250 millivolts
-const int TABLE_ENTRIES = 12;
-const int firstElement = 250; // first entry is 250 mV
-const int INTERVAL  = 250; // millivolts between each element
-static int distance[TABLE_ENTRIES] = {150,140,130,100,60,50,40,35,30,25,20,15};
-
-int getDistance(int mV)
-{
-   if( mV >  INTERVAL * TABLE_ENTRIES-1 )
-      return distance[TABLE_ENTRIES-1];
-   else
-   {
-      int index = mV / INTERVAL;
-      float frac = (mV % 250) / (float)INTERVAL;
-      return distance[index] - ((distance[index] - distance[index+1]) * frac);
-   }
-}
 
