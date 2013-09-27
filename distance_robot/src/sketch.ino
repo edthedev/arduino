9306043061289c5458d112e5365dc9e0d22ff366
Edward <edthedev@gmail.com>
2013-09-26 20:31:55 -0500

    Made code to cause a robot to spin in circles when told to go forward, if it's distance sensor is triggering. So the robot cannot hit things. 	This is really great if you happen to have a two year old running 	the remote control.

---
+++ distance_robot/src/sketch.ino
@@ -0,0 +1,95 @@
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com/
+ on 10 Nov 2012.
+ */
+
+
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+#define LEDPin 13 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+#define to_motor 0
+#define from_remote 12
+
+int go_forth = 0;
+
+void setup() {
+
+ pinMode(to_motor, OUTPUT);
+ pinMode(from_remote, INPUT);
+
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+}
+
+void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+
+// Best not to drive an calculate...
+ digitalWrite(to_motor, LOW);
+
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ // digitalWrite(LEDPin, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ // digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
+
+ go_forth = digitalRead(from_remote);
+ if(duration > 0)
+ {
+	 go_forth = 0;
+	 digitalWrite(LEDPin, HIGH);
+ }
+ else
+ {
+	 digitalWrite(LEDPin, LOW);
+ }
+ if(go_forth)
+ {
+	 digitalWrite(to_motor, HIGH);
+ }
+ else
+ {
+ }
+
+
+}
+
+
