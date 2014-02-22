73a0229956b118d0d660eb48d5bd451f75eae2ac
Edward Delaporte <edthedev@gmail.com>
2014-02-21 23:18:53 -0600

    Strobe works with foreach loop.

---
+++ bargraph/bargraph.ino
@@ -0,0 +1,43 @@
+/*
+LEDs sketch
+Blink three LEDs each connected to a different digital pin
+*/
+const int mydelay = 200;
+
+#define foreach(item, array) \
+	for(int keep = 1, \
+	count = 0,\
+	size = sizeof (array) / sizeof *(array); \
+	keep && count != size; \
+	keep = !keep, count++) \
+	for(item = (array) + count; keep; keep = !keep)
+
+const int bar_pins[] = {3,4,5,6,7,8,9,10,11,12};
+const int bar_pin_count = 10;
+
+void setup()
+{
+
+// Flash for heartbeat.
+pinMode(13, OUTPUT);
+
+// Serial.begin(9600);
+
+// declare LED pins as output
+  for(int led = bar_pins[0]; led <=  bar_pins[0] + bar_pin_count; led++)
+  {
+    pinMode(led, OUTPUT);
+  }
+
+}
+void loop()
+{
+	foreach(const int *p, bar_pins)
+	{
+		int pin = *p;
+		digitalWrite(pin, LOW);
+		delay(mydelay);
+		digitalWrite(pin, HIGH);
+	}
+
+}
