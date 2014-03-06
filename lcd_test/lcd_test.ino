a8772427888acf734ab04eeefb8a826d9c7465e1
Edward Delaporte <edthedev@gmail.com>
2014-03-05 22:06:39 -0600

    Finished testing the 16 button floppy keypad. Not too reliable...could need debouncing.

--- lcd_test/lcd_test.ino
+++ lcd_test/lcd_test.ino
@@ -4,17 +4,37 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+
+int values[4];
+int pin[4];
+
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+	lcd.begin(16, 2);
+
+	pinMode(A1, OUTPUT);
+
+	pin[0] = A2;
+	pin[1] = A3;
+	pin[2] = A4;
+
+    for(int i=0; i<5; i++)
+	{
+		pinMode(pin[i], INPUT);
+	}
+
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+
+	digitalWrite(A1, HIGH);
+	pinMode(A1, INPUT);
+
+    for(int i=0; i<5; i++)
+	{
+		lcd.setCursor(i, 0);
+		int my_value = digitalRead(pin[i]);
+		lcd.print(my_value);
+	}
+	lcd.setCursor(0, 1);
+	lcd.print(millis()/1000);
 }
